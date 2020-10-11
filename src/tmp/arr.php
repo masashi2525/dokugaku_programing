df3d4a24a52f2f8192e7eccecdc94b3760bf4d62
masashi2525 <noerunoe8@gmail.com>
2020-10-11 16:31:26 +0900

    連想配列練習

--- src/tmp/arr.php
+++ src/tmp/arr.php
@@ -1,9 +1,19 @@
 <?php
 
-$comics = ['火の鳥', 'ガラスの仮面'];
-var_export($comics);
+$user = [
+  'name' => 'kume',
+  'sex' => 'male',
+];
 
-$comics[] = 'BANANA FISH';
-var_export($comics);
+$user['age'] = 20;
 
-echo $comics[2] . PHP_EOL;
+var_export($user);
+echo $user['name'] . PHP_EOL;
+
+// $comics = ['火の鳥', 'ガラスの仮面'];
+// var_export($comics);
+
+// $comics[] = 'BANANA FISH';
+// var_export($comics);
+
+// echo $comics[2] . PHP_EOL;
